7db7204da408666039a81983c578d360514c7cc8
Robin Wilborn <rtwilborn@gmail.com>
2016-07-12 10:55:20 -0700

    modify syntactic sugar to use pos

--- solutions/w2/w2d4/human_player.rb
+++ solutions/w2/w2d4/human_player.rb
@@ -8,21 +8,21 @@ class HumanPlayer
 
   def get_move
     puts "Where would you like to move? (row, col)"
-    gets.chomp.split(", ").map(&:to_i)
+    gets.chomp.split(",").map(&:to_i)
   end
 
   def display(board)
     row0 = "0 |"
     (0..2).each do |col|
-      row0 << (board.empty?([0, col]) ? "   |" : " " + board[0, col].to_s + " |")
+      row0 << (board.empty?([0, col]) ? "   |" : " " + board[[0, col]].to_s + " |")
     end
     row1 = "1 |"
     (0..2).each do |col|
-      row1 << (board.empty?([1, col]) ? "   |" : " " + board[1, col].to_s + " |")
+      row1 << (board.empty?([1, col]) ? "   |" : " " + board[[1, col]].to_s + " |")
     end
     row2 = "2 |"
     (0..2).each do |col|
-      row2 << (board.empty?([2, col]) ? "   |" : " " + board[2, col].to_s + " |")
+      row2 << (board.empty?([2, col]) ? "   |" : " " + board[[2, col]].to_s + " |")
     end
 
     puts "    0   1   2  "
