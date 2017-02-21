8152ef9e9a3f3e8cade21ec788cc1ab1e96d145f
Robin Wilborn <rtwilborn@gmail.com>
2017-02-21 22:27:37 +0000

    Finished W2D3

--- w2/w2d3/exercises/lib/io_exercises.rb
+++ w2/w2d3/exercises/lib/io_exercises.rb
@@ -10,3 +10,44 @@
 #   shuffles the lines, and saves it to the file "{input_name}-shuffled.txt". You
 #   could create a random number using the Random class, or you could use the
 #   `shuffle` method in array.
+
+def guessing_game
+    
+    guessed = false
+    guesses = 0
+    the_number = Random.rand(1..100)
+    
+    until guessed
+        guesses += 1
+        puts "Guess a number"
+        current_guess = gets.chomp.to_i
+        puts "You guessed #{current_guess}"
+        puts "Guesses: #{guesses}"
+        guessed = true if current_guess == the_number
+        puts "too high" if current_guess > the_number
+        puts "too low" if current_guess < the_number
+        
+    end
+    puts "The number was #{the_number}."
+    puts "It took #{guesses} tries!"
+end
+
+def shuffler
+    
+    file_name = ""
+    puts "Enter a file"
+    file_name = gets.chomp
+    stripped = file_name.split(".")
+    shuffled_file = "#{stripped[0]}-shuffled.txt"
+    
+    file = File.open(file_name, "r")
+    contents = file.readlines
+    shuffled = contents.shuffle
+    
+    new_file = File.open(shuffled_file, "w")
+    shuffled.each { |line| new_file.puts line }
+    
+    new_file.close
+    file.close
+end
+
